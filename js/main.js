984cd95718cf237e87874f37b6b3a781dfde5229
hyunil <pintory@gmail.com>
2015-03-26 11:51:07 -0700

    Update scroll function
    scroll function

--- js/main.js
+++ js/main.js
@@ -1,19 +1,30 @@
 $(document).ready(function(){
-	
 	//Check to see if the window is top if not then display button
 	$(window).scroll(function(){
-		if ($(this).scrollTop() > 600) {
-			$('h1').css("margin-left","0");
-		} else {
-			$('h1').css("margin-left","-500px");
+		
+		// Initialize top position of element
+		var position_element_2 = $('.element_2').offset().top;
+
+		// compare if current position of window is greater than top position of element.
+		if ($(this).scrollTop()+ $(window).height() >= position_element_2  ) {
+			$('h1').css('color','#a0F219'); // change css attribute
+			// do animate after .8 sec
+			$('p').delay(800).animate({
+				opacity: 1
+				}, 400 ); // duration is .4 sec
+		}
+		// else if, that mean smaller than top position of element, 
+		else {
+			// change css attributes
+			$('h1').css('color','#382793'); 
+			$('p').css('opacity','0'); 
 		}
 
-		if ($(this).scrollTop() > 1500) {
+		var position_element_3 = $('.element_3').offset().top;
+		if ($(this).scrollTop() >= position_element_3 ) {
 			$('.col-md-4').fadeIn();
 		} else {
 			$('.col-md-4').fadeOut();
 		}
-
 	});
-	
 });
